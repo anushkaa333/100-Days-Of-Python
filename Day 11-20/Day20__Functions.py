86f828f937e6eed1b619b2b3b44d0880ad54eac3
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2022-12-24 07:24:17 +0530

    Add files via upload

---
+++ Day 11-20/Day20__Functions.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    c = a+b
+    # print("sum is", c)
+    return c
+
+
+def comp(a, b):
+    if a>b:
+        print("a is greater")
+    else:
+        print("b is greater")
+
+
+d = add(6, 7)
+print("sum ",d)
+comp(6, 7)
+
+#sum  13
+# b is greater
