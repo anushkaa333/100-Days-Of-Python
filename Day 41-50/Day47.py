c40f01a11b27a4bf460739f63e4c99ffd908c529
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2023-01-21 23:53:02 +0530

    Add files via upload

---
+++ Day 41-50/Day47.py
@@ -0,0 +1,45 @@
+
+while True:
+    print("Encrypt - Decrypt program")
+    user = int(input("1. Encrypt  2.Decrypt  3.Quit"))
+    if(user==1):
+        user1 = input("Enter your message")
+        
+        while True:
+            try:
+                key = int(input("Enter a key to encrypt"))
+                if (key<0 or key>0):
+                    break
+            except Exception as e:
+                print("Enter only digits")
+            
+        ans1 = encrypt(user1, key)
+        print(f"Encrypted text {ans1}")
+
+    if(user==2):
+        user2 = input("Enter your message to decrypt")
+        
+        while True:
+            try:
+                key = int(input("Enter a key given to encrypt"))
+                if (key<0 or key>0):
+                    break
+            except Exception as e:
+                print("Enter only digits")
+            
+        ans2 = decrypt(user2, key)
+        print(f"Decrypt text {ans2}")
+
+
+    if (user == 3):
+        break
+
+
+
+
+
+
+
+
+
+
