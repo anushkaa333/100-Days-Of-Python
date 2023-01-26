dee52eb085aef55d67aa2e38451eb4c75f01cc7d
anushkaa333 <68331130+anushkaa333@users.noreply.github.com>
2023-01-27 00:44:20 +0530

    Add files via upload

---
+++ Day 51-60/Day51/Day51.py
@@ -0,0 +1,40 @@
+#******************** I]
+# r+ => Both read and write 
+
+# f = open("myfile.txt", "r+")
+
+# f.write("My name is Anushka")
+
+# f.seek(10)
+
+# print(f.tell())
+# #10
+
+# data = f.read(3)
+# print(data)
+# #An
+
+# f.close()
+
+
+#********************* II]
+# a+ => Both read and append 
+
+
+p = open("myfile.txt1", "a+")
+
+data = p.read(3)
+print(data)
+#This wont display any result as file pointer is at end due to append fuctionality for this use p.seek(0) to move file pointer at start
+
+p.seek(0)
+text = p.read(10)
+print(text)
+#Anushka
+#BE
+
+t = p.truncate(5) #Truncate the file to 1st 5 bytes
+print(t)
+#Anush
+
+p.close()
